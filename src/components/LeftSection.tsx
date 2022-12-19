1ab756fe31d4c770360218da7d4c9897201d08e6
Max Omeleneckii <max.omeleneckii@gmail.com>
2022-12-19 17:01:19 +0300

    feat: add information about Experience

--- src/components/LeftSection.tsx
+++ src/components/LeftSection.tsx
@@ -1,4 +1,5 @@
 import { Divider, List, ListItem, ListItemText, Typography } from '@mui/material';
+import styled from 'styled-components';
 import { Section } from './styledComponents/styledComponents';
 
 export const leftSectionData = {
@@ -10,6 +11,11 @@ export const leftSectionData = {
     description: '',
   },
 };
+
+const ListItemCustom = styled.li`
+  padding: 0px;
+  margin-left: 15px;
+`;
 
 export const LeftSection = () => {
   return (
@@ -43,9 +49,34 @@ export const LeftSection = () => {
               }
             />
             <List sx={{ width: '100%' }}>
-              <ListItem>
-                <ListItemText>20.12.2022</ListItemText>
-                <ListItemText>Content</ListItemText>
+              <ListItem sx={{ paddingLeft: '0px', columnGap: '30px', alignItems: 'flex-start' }}>
+                <ListItemText sx={{ flex: '1 0 100px' }}>05.06.2021 - 06.12.2022</ListItemText>
+                <ListItemText>
+                  <Typography component="h3" sx={{ fontWeight: 'bold' }}>
+                    Frontend Developer
+                  </Typography>
+                  <Typography component="h4" sx={{ fontStyle: 'italic' }}>
+                    RSSchool (student)
+                  </Typography>
+                  <List sx={{ listStyle: 'disc' }}>
+                    <ListItemCustom>
+                      Implemented websites and browser games using HTML, CSS (SCSS), JS, TS.
+                    </ListItemCustom>
+                    <ListItemCustom>
+                      Used the React library in development in conjunction with MUI, styled
+                      components, modular styles, Redux and much more.
+                    </ListItemCustom>
+                    <ListItemCustom>
+                      Participated in the team development of web applications using TS and React.
+                    </ListItemCustom>
+                    <ListItemCustom>
+                      Applied class and functional approaches in development.
+                    </ListItemCustom>
+                    <ListItemCustom>
+                      Participated in the development of these projects:
+                    </ListItemCustom>
+                  </List>
+                </ListItemText>
               </ListItem>
             </List>
           </ListItem>
